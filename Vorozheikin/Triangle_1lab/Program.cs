86bc5495e76448de01508aca2cc4dd86a848e122
KIchaITaIl <53560250+KIchaITaIl@users.noreply.github.com>
2023-09-30 14:37:08 +0700

    Add files via upload

---
+++ Vorozheikin/Triangle_1lab/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Microsoft.VisualBasic;
+using Triangle_1lab;
+
+void TriangleExec (string A, string B, string C)
+{
+    double a = Convert.ToDouble (A);
+    double b = Convert.ToDouble (B);
+    double c = Convert.ToDouble (C);
+    var result = TriangleCalc.CalculateTriangleTypeAndCoordinates(a, b, c);
+    string TriangleTypeResPer = Convert.ToString(result.Item1);
+    string TriangleTypeRes(string TriangleTypeResPer)
+    {
+        return TriangleTypeResPer;
+    }
+
+}
+
+TriangleExec("10", "15", "20");
+
+//var result = TriangleCalc.CalculateTriangleTypeAndCoordinates(a, b, c);
+
+//Console.WriteLine($"Тип треугольника: {result.Item1}");
+//Console.WriteLine("Координаты вершин:");
+//foreach (var coordinate in result.Item2)
+//{
+//    Console.WriteLine($"({coordinate.Item1}, {coordinate.Item2})");
+//}
---
+++ Vorozheikin/Triangle_1lab/TriangleCalc.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle_1lab
+{
+    internal class TriangleCalc
+    {
+        public enum TriangleType
+        {
+            NotTriangle,
+            Equilateral,
+            Isosceles,
+            Scalene
+        }
+
+        public static Tuple<TriangleType, List<Tuple<int, int>>> CalculateTriangleTypeAndCoordinates(double a, double b, double c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0)
+            {
+                Console.WriteLine("Invalid input. All sides must be positive.");
+                return Tuple.Create(TriangleType.NotTriangle, new List<Tuple<int, int>>());
+            }
+
+            if (!(a + b > c && a + c > b && b + c > a))
+            {
+                Console.WriteLine("The input values do not form a triangle.");
+                return Tuple.Create(TriangleType.NotTriangle, new List<Tuple<int, int>>());
+            }
+
+            List<Tuple<int, int>> scaledCoordinates = ScaleCoordinates(a, b, c);
+
+            if (a == b && b == c)
+            {
+                Console.WriteLine("Equilateral triangle");
+                return Tuple.Create(TriangleType.Equilateral, scaledCoordinates);
+            }
+            else if (a == b || a == c || b == c)
+            {
+                Console.WriteLine("Isosceles triangle");
+                return Tuple.Create(TriangleType.Isosceles, scaledCoordinates);
+            }
+            else
+            {
+                Console.WriteLine("Scalene triangle");
+                return Tuple.Create(TriangleType.Scalene, scaledCoordinates);
+            }
+
+        }
+
+        private static List<Tuple<int, int>> ScaleCoordinates(double a, double b, double c)
+        {
+            double maxSide = Math.Max(a, Math.Max(b, c));
+            double scalingFactor = 100 / maxSide;
+            var scaledCoordinates = new List<Tuple<int, int>>();
+            scaledCoordinates.Add(Tuple.Create((int)(a * scalingFactor), (int)(a * scalingFactor)));
+            scaledCoordinates.Add(Tuple.Create((int)(b * scalingFactor), (int)(b * scalingFactor)));
+            scaledCoordinates.Add(Tuple.Create((int)(c * scalingFactor), (int)(c * scalingFactor)));
+            return scaledCoordinates;
+        }
+    }
+}
