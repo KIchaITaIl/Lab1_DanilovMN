e18cd6a8007e588f2a73c27497e78fb3f9c5fb16
KIchaITaIl <53560250+KIchaITaIl@users.noreply.github.com>
2023-10-07 14:38:03 +0700

    Add files via upload

--- Vorozheikin/Triangle_1lab/Program.cs
+++ Vorozheikin/Triangle_1lab/Program.cs
@@ -2,29 +2,66 @@
 
 
 using Microsoft.VisualBasic;
+using Serilog;
 using Triangle_1lab;
+using static Triangle_1lab.TriangleCalc;
 
-void TriangleExec (string A, string B, string C)
+string template = "{Timestamp:HH:mm:ss} | [{Level:u3}] | {Message:lj}{NewLine}{Exception}";
+Log.Logger = new LoggerConfiguration().MinimumLevel.Debug()
+            .WriteTo.Console(outputTemplate: template)
+            .WriteTo.File("G:\\DanilovMN\\Vorozheikin\\Triangle_1lab\\File.txt", outputTemplate: template)
+            .CreateLogger();
+
+Log.Verbose("Логгер сконфигурирован");
+Log.Information("Приложение запущено");
+
+Tuple<TriangleCalc.TriangleType, List<Tuple<int, int>>> TriangleExec(string A, string B, string C)
 {
-    double a = Convert.ToDouble (A);
-    double b = Convert.ToDouble (B);
-    double c = Convert.ToDouble (C);
-    var result = TriangleCalc.CalculateTriangleTypeAndCoordinates(a, b, c);
-    string TriangleTypeResPer = Convert.ToString(result.Item1);
-    string TriangleTypeRes(string TriangleTypeResPer)
+    Log.Information("Запущен метод запуска программы");
+    bool checkA = Double.TryParse(A, out var numberA);
+    Log.Debug("Выполнение проверки преобразования строки A в double");
+    if (checkA == true)
     {
-        return TriangleTypeResPer;
+        Log.Information("Cтрока А может быть преобразована в тип double");
+    }
+    else
+    {
+        Log.Error("Строка А не может быть преобразована в тип double!");
+        return Tuple.Create(TriangleType.Invalide, new List<Tuple<int, int>>-2, -2());
     }
 
+    double a = Convert.ToDouble(A);
+    Log.Verbose("Строка А преобразована в тип double");
+    bool checkB = Double.TryParse(B, out var numberB);
+    Log.Debug("Выполнение проверки преобразования строки B в double");
+    if (checkB == true)
+    {
+        Log.Information("Cтрока B может быть преобразована в тип double");
+    }
+    else
+    {
+        Log.Error("Строка B не может быть преобразована в тип double!");
+        return 
+    }
+
+    double b = Convert.ToDouble(B);
+    Log.Verbose("Строка B преобразована в тип double");
+    bool checkC = Double.TryParse(C, out var numberC);
+    Log.Debug("Выполнение проверки преобразования строки C в double");
+    if (checkC == true)
+    {
+        Log.Information("Cтрока C может быть преобразована в тип double");
+    }
+    else
+    {
+        Log.Error("Строка C не может быть преобразована в тип double!");
+    }
+    double c = Convert.ToDouble(C);
+    Log.Verbose("Строка C преобразована в тип double");
+
+    var result = TriangleCalc.CalculateTriangleTypeAndCoordinates(a, b, c);
+    Log.Verbose("Вычисление вида треугольника и координат его вершин успешно выполнено!");
+    return result;
 }
 
-TriangleExec("10", "15", "20");
-
-//var result = TriangleCalc.CalculateTriangleTypeAndCoordinates(a, b, c);
-
-//Console.WriteLine($"Тип треугольника: {result.Item1}");
-//Console.WriteLine("Координаты вершин:");
-//foreach (var coordinate in result.Item2)
-//{
-//    Console.WriteLine($"({coordinate.Item1}, {coordinate.Item2})");
-//}
+TriangleExec("-10", "15", "20");
--- Vorozheikin/Triangle_1lab/TriangleCalc.cs
+++ Vorozheikin/Triangle_1lab/TriangleCalc.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Serilog;
 
 namespace Triangle_1lab
 {
+    
     internal class TriangleCalc
     {
         public enum TriangleType
@@ -13,38 +15,47 @@ namespace Triangle_1lab
             NotTriangle,
             Equilateral,
             Isosceles,
-            Scalene
+            Scalene,
+            Invalide
         }
+
+        
 
         public static Tuple<TriangleType, List<Tuple<int, int>>> CalculateTriangleTypeAndCoordinates(double a, double b, double c)
         {
+            Log.Verbose("Метод вычисления вида треугольника запущен");
+
             if (a <= 0 || b <= 0 || c <= 0)
             {
-                Console.WriteLine("Invalid input. All sides must be positive.");
+                Log.Warning("Некорректный ввод данных, числа должны быть положительными!");
+                
                 return Tuple.Create(TriangleType.NotTriangle, new List<Tuple<int, int>>());
             }
 
             if (!(a + b > c && a + c > b && b + c > a))
             {
+                Log.Warning("Введенные данные не формируют треугольник");
+
                 Console.WriteLine("The input values do not form a triangle.");
                 return Tuple.Create(TriangleType.NotTriangle, new List<Tuple<int, int>>());
             }
 
             List<Tuple<int, int>> scaledCoordinates = ScaleCoordinates(a, b, c);
+            Log.Verbose("Метод вычисления координат вершин треугольника успешно выполнен");
 
             if (a == b && b == c)
             {
-                Console.WriteLine("Equilateral triangle");
+                Log.Information("Равностороний треугольник");
                 return Tuple.Create(TriangleType.Equilateral, scaledCoordinates);
             }
             else if (a == b || a == c || b == c)
             {
-                Console.WriteLine("Isosceles triangle");
+                Log.Information("Равнобедренный треугольник");
                 return Tuple.Create(TriangleType.Isosceles, scaledCoordinates);
             }
             else
             {
-                Console.WriteLine("Scalene triangle");
+                Log.Information("Неравносторонний треугольник");
                 return Tuple.Create(TriangleType.Scalene, scaledCoordinates);
             }
 
@@ -52,12 +63,19 @@ namespace Triangle_1lab
 
         private static List<Tuple<int, int>> ScaleCoordinates(double a, double b, double c)
         {
+            Log.Verbose("Запущен метод вычисление координат вершин треугольника");
             double maxSide = Math.Max(a, Math.Max(b, c));
+            Log.Debug("Вычисление длинной стороны");
             double scalingFactor = 100 / maxSide;
+            Log.Debug("Вычисление коэффициента масштабирования");
             var scaledCoordinates = new List<Tuple<int, int>>();
+            Log.Debug("Создание списка координат вершин треугольника");
             scaledCoordinates.Add(Tuple.Create((int)(a * scalingFactor), (int)(a * scalingFactor)));
+            Log.Debug("Вычисление координаты вершины А");
             scaledCoordinates.Add(Tuple.Create((int)(b * scalingFactor), (int)(b * scalingFactor)));
+            Log.Debug("Вычисление координаты вершины B");
             scaledCoordinates.Add(Tuple.Create((int)(c * scalingFactor), (int)(c * scalingFactor)));
+            Log.Debug("Вычисление координаты вершины C");
             return scaledCoordinates;
         }
     }
